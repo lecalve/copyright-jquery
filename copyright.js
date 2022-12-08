bd0820ba9f9a31697f5cd435dcc9083edba27e48
lecalve <97450691+lecalve@users.noreply.github.com>
2022-12-08 10:30:26 -0300

    Another approach (skipping img URL's)

--- copyright.js
+++ copyright.js
@@ -1,13 +1,17 @@
 (function ($) {
     $(document).ready(function() {
-        $("strong:nth-child(2)").each(function() {
-            $(this).html($(this).html().replace(/©/g,"<span style='font-family:Arial Black;font-weight:700;'>©</span>"));
+        $("p,li").filter(function() {
+            return !$(this).find('img:first').length;
+        })
+        .html(function(index, old) {
+            return old.replace(/©/g, "<span style='font-family:Arial Black;font-weight:700;'>©</span>");
         });
-        $("strong:nth-child(1)").each(function() {
-            $(this).html($(this).html().replace(/®/g,"<span style='font-family:Arial;font-weight:700;font-size:14px;'>®</span>"));
-        });
-        $("li").each(function() {
-            $(this).html($(this).html().replace(/©/g,"<span style='font-family:Arial Black;font-weight:700;'>©</span>"));
+        $("p").filter(function() {
+            return !$(this).find('img:first').length;
+        })
+        .html(function(index, old) {
+            return old.replace(/®/g, "<span style='font-family:Arial Black;font-weight:700;'>®</span>");
         });
     })
+
 })(jQuery);
